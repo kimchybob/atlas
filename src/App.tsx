7d983d4207841f902c1121a875b55ebffb64b3a1
kimchy <kimchybob>
2024-02-16 23:32:23 +1030

    add result list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import ResultList from './components/ResultList';
 import { useState } from 'react';
 import axios from 'axios';
+import Button from '@mui/material/Button';
 
 function App() {
   const [results, setResults] = useState<any[]>([]);
 
   const onSearch = (keyword: string) => {
-    axios.get(`http://bie.ala.org.au/ws/search.json?amp;facets=idxtype,rank,speciesGroup,imageAvailable&rows=100&q=${keyword}`)
+    axios.get(`http://bie.ala.org.au/ws/search.json?q=${keyword}`)
       .then(response => {
-        setResults(response.data.searchResults.results);
-        console.log(results);
+        setResults(response.data?.searchResults?.results);
       })
       .catch(error => {
         console.error(error);
@@ -21,6 +22,8 @@ function App() {
   return (
     <div className="App">
       <SearchBar onSearch={onSearch} />
+      <Button variant="contained" >Download</Button>
+      <ResultList searchResults={results}/>
     </div>
   );
 }
